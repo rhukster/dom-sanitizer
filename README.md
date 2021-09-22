e88e914ac405cfc303edd31440621971b5e33902
Andy Miller <rhuk@mac.com>
2021-09-22 16:10:09 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# dom-sanitizer
+# DOMSanitizer
+
 A simple but effective DOM/SVG/MathML Sanitizer for PHP 7.4+
+
+## Installation
+
+```sh
+composer require rhukster/dom-sanitizer
+```
+
+## Usage
+
+### Sanitizing HTML
+
+```php
+require Rhukster/DomSanitizer/DomSanitizer
+
+$bad_html = file_get_contents('bad.html');
+$sanitizer = new DOMSanitizer();
+$good_html = $sanitizer->sanitize($bad_html);
+```
+
+### Sanitizing SVG
+
+```php
+require Rhukster/DomSanitizer/DomSanitizer
+
+$bad_svg = file_get_contents('bad.svg');
+$sanitizer = new DOMSanitizer(DOMSanitizer::SVG);
+$good_svg = $sanitizer->sanitize($bad_svg);
+```
---
+++ tests/DomSanitizerTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Rhukster\DomSanitizer;
+
+use PHPUnit\Framework\TestCase;
+
+final class DomSanitizerTest extends TestCase
+{
+    public function testDomSanitizerInstance(): void
+    {
+        $instance = new DOMSanitizer();
+        $this->assertInstanceOf(
+            DomSanitizer::class,
+            $instance
+        );
+    }
+    
+    public function testCompromisedSVG(): void
+    {
+        $bad_svg = file_get_contents('./tests/bad.svg');
+        $good_svg = file_get_contents('./tests/good.svg');
+
+        $sanitizer = new DOMSanitizer(DOMSanitizer::SVG);
+
+        $this->assertEquals(
+            $good_svg,
+            $sanitizer->sanitize($bad_svg)
+        );
+    }
+
+
+}
