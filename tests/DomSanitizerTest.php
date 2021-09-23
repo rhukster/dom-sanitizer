9e39db2d2dca26239abb74bef01863234d89d98d
Andy Miller <rhuk@mac.com>
2021-09-23 14:46:07 -0600

    more tests

--- tests/DomSanitizerTest.php
+++ tests/DomSanitizerTest.php
@@ -30,8 +30,21 @@ final class DomSanitizerTest extends TestCase
             $good_html,
             $cleaned
         );
-}
-    
+    }
+
+    public function testHTMLSnippet(): void
+    {
+        $sanitizer = new DOMSanitizer();
+
+        $input = '<div><p class="foo" onclick="alert(\'danger\');">bar</p><script>alert(\'more danger\')</script></div>';
+        $expected = '<div><p class="foo">bar</p></div>';
+
+        $this->assertEqualHTML(
+            $expected,
+            $sanitizer->sanitize($input)
+        );
+    }
+
     public function testCompromisedSVG(): void
     {
         $bad_svg = file_get_contents('./tests/bad.svg');
@@ -41,6 +54,65 @@ final class DomSanitizerTest extends TestCase
         $this->assertEqualHtml(
             $good_svg,
             $sanitizer->sanitize($bad_svg)
+        );
+    }
+
+    public function testCustomTags(): void
+    {
+        $sanitizer = new DOMSanitizer();
+
+        $input = '<div><foo>testing</foo></div>';
+        $expected = '<div></div>';
+
+        $this->assertEqualHTML(
+            $expected,
+            $sanitizer->sanitize($input)
+        );
+
+        $expected2 = '<div><foo>testing</foo></div>';
+        $sanitizer->addAllowedTags(['foo']);
+
+        $this->assertEqualHTML(
+            $expected2,
+            $sanitizer->sanitize($input)
+        );
+
+        $expected3 = '<div></div>';
+        $sanitizer->addDisallowedTags(['foo']);
+
+        $this->assertEqualHTML(
+            $expected3,
+            $sanitizer->sanitize($input)
+        );
+    }
+
+
+    public function testCustomAttributes(): void
+    {
+        $sanitizer = new DOMSanitizer();
+
+        $input = '<div><p blah="something">testing</p></div>';
+        $expected = '<div><p>testing</p></div>';
+
+        $this->assertEqualHTML(
+            $expected,
+            $sanitizer->sanitize($input)
+        );
+
+        $expected2 = '<div><p blah="something">testing</p></div>';
+        $sanitizer->addAllowedAttributes(['blah']);
+
+        $this->assertEqualHTML(
+            $expected2,
+            $sanitizer->sanitize($input)
+        );
+
+        $expected3 = '<div><p>testing</p></div>';
+        $sanitizer->addDisallowedAttributes(['blah']);
+
+        $this->assertEqualHTML(
+            $expected3,
+            $sanitizer->sanitize($input)
         );
     }
 
